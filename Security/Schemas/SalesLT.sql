c4145af68c5b03f6e85433afc16d84f2215e1bc8
Jordan-Sanders <83335543+Jordan-Sanders@users.noreply.github.com>
2025-11-13 09:30:25 +0200

    Delete Security directory

--- Security/Schemas/SalesLT.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE SCHEMA [SalesLT]
-GO
-
-EXEC sys.sp_addextendedproperty N'MS_Description', N'Contains objects related to products, customers, sales orders, and sales territories.', 'SCHEMA', N'SalesLT'
-GO
