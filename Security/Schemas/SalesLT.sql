d68f38ebbddf6ec87ed83093cd265366174d42c1
DmitryB <dmitryb@devart.com>
2025-11-13 09:20:20 +0200

    1

---
+++ Security/Schemas/SalesLT.sql
@@ -0,0 +1,5 @@
+﻿CREATE SCHEMA [SalesLT]
+GO
+
+EXEC sys.sp_addextendedproperty N'MS_Description', N'Contains objects related to products, customers, sales orders, and sales territories.', 'SCHEMA', N'SalesLT'
+GO
